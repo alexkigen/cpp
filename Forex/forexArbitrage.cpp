1306370200cb8330a6e9ff459c8a09b9373809ec
Alex Kigen <61216190+alexkigen@users.noreply.github.com>
2020-03-03 14:34:33 -0500

    Create forexArbitrage.cpp

---
+++ Forex/forexArbitrage.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+using namespace std;
+
+double SpotArbitrage (double bid_, double offer_) {
+	
+	double spread = bid_ - offer_;
+	double payOff;
+	
+	if (bid_ > offer_) {
+		payOff = spread;
+	} else {
+		payOff = 0;
+	}
+	
+	return payOff;
+}
+
+double triangularArbitrage (double bidPairA, double offerPairA,
+							double bidPairB, double offerPairB_)
+{
+								
+}
+
+double notionalCall () {
+	double notional;
+	cout << "What notional would you like?" << endl;
+	cout << "Notional = ";
+	cin >> notional;
+	return notional;
+}
+
+int main () {
+	double bid;
+	double offer;
+	double payOff;
+	double notional;
+	double profitArbitrage;
+	
+	cout << "What are the bid and offer prices?" << endl;
+	cout << "Bid: ";
+	cin >> bid;
+	cout << "Offer: ";
+	cin >> offer;
+	
+	payOff = SpotArbitrage(bid, offer);
+	cout << payOff << endl;
+	
+	if (payOff > 0) {
+		notional = notionalCall();
+		profitArbitrage = payOff * notional;
+		cout << "Expected Profit: " << profitArbitrage << endl;
+	} else {
+		cout << "No arbitrage opportunity";
+	}
+	
+}
