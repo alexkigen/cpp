3235e159e77dda6fec78c55110c7da8aa5f73036
Alex Kigen <61216190+alexkigen@users.noreply.github.com>
2020-03-21 11:31:22 -0400

    Update classes.cpp

--- practicals/classes.cpp
+++ practicals/classes.cpp
@@ -1,31 +1,53 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 
-using namespace std;
+//create a patch to correct system error not recognizing to_string as a member of std
 
+namespace patch
+{
+    template < typename T > std::string to_string( const T& n )
+    {
+        std::ostringstream stm;
+        stm << n ;
+        return stm.str() ;
+    }
+}
+
+//create a class called employees
 class Employees {
 	private:
-		string company;
+		std::string company;
 		
 	public:
+		std::string empName;
 		int age;
 		double salary;
 		int serviceYears;
-		string dept;
+		std::string dept;
 		
-		void getCompany(string companyInput) {
+		void getCompany(std::string companyInput) {
 			company = companyInput;
 		}
 		
-		string printDetails () {
-			return ("works for " + company + " in "+ dept);
+		std::string toString (int nonString) {
+			std::string stringConv = patch::to_string(nonString);
+			return stringConv;
 		}
+		
+		std::string printDetails () {
+			return (empName + " is age " + toString(age) + " and has worked for " + company + " in "+ dept + " for " + toString(serviceYears) + " years");
+		}
+		
 };
+
+using namespace std;
 
 int main () {
 	Employees Susan;
 	
 	Susan.getCompany("LOLO");
+	Susan.empName = "Susan";
 	Susan.age = 20;
 	Susan.dept = "Marketing";
 	Susan.salary = 125000;
