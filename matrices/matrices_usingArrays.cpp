2b56972f20d0f03d03fc63095e5364fb1f0bc413
Alex Kigen <61216190+alexkigen@users.noreply.github.com>
2020-03-22 01:31:21 -0400

    Update matrices_usingArrays.cpp

--- matrices/matrices_usingArrays.cpp
+++ matrices/matrices_usingArrays.cpp
@@ -1,1 +1,55 @@
+//working with matrices in C++
 
+#include <iostream>
+
+using namespace std;
+
+//define the dimensions of the matrix (preprocessor directives)
+
+#define NROW 4
+#define NCOL 3
+
+typedef double MATRIX [NROW][NCOL];
+typedef double VECTOR [NROW];
+
+//function to print matrix
+
+void printMatrix (MATRIX matrix_) {
+
+    for (int i = 0; i < NROW; ++i) {
+        
+        cout << endl << " ";
+
+        for (int j = 0; j < NCOL; ++j) {
+
+            cout << matrix_[i][j] << " ";
+        }
+//        cout << ")";
+    }
+    cout << endl;
+}
+
+int main () {
+
+    MATRIX matA;
+
+    //first row of matrix
+    matA[0][0] = 1;
+    matA[0][1] = 2;
+    matA[0][2] = 3;
+    //second row
+    matA[1][0] = 1;
+    matA[1][1] = 2;
+    matA[1][2] = 3;
+    //third row
+    matA[2][0] = 1;
+    matA[2][1] = 2;
+    matA[2][2] = 3;
+    //fourth row
+    matA[3][0] = 1;
+    matA[3][1] = 2;
+    matA[3][2] = 3;
+
+//print(matA);
+    printMatrix(matA);
+}
